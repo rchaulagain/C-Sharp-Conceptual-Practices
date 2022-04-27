6f13c51314bb4d6b51044757eb5a6987d99c6dfb
Rupa <90302628+rougedao@users.noreply.github.com>
2022-04-27 09:57:24 +0545

    Initial development. 1. Added sample example for stack.

---
+++ Console_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Console_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+
+            #region How to stack the obects and read
+            var stack_Practice = new Stack_Practice();
+            stack_Practice.StackObjects(); // first stack the objets
+            stack_Practice.PrintStackObjects(); // now we print the stacked objects
+            #endregion How to stack the obects and read
+
+        }
+    }
+}
---
+++ Console_Practice/Stack_Practice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Practice
+{
+    public class Stack_Practice
+    {
+        public Stack_Practice()
+        {
+
+        }
+
+        public Stack<string> StackObjects()
+        {
+            var stackObj = new Stack<string>();
+            stackObj.Push("1st Item in the stack.");
+            stackObj.Push("2nd Item in the stack.");
+            stackObj.Push("3rd Item in the stack.");
+            stackObj.Push("4th Item in the stack.");
+            return stackObj;
+        }
+        public void PrintStackObjects()
+        {
+            Console.WriteLine("\t \t \t-------Stack Practice Sample--------");
+            Console.WriteLine("\n \t Total Stack Items Count : {0}", StackObjects().Count);
+            Console.WriteLine("\t Stacked Items");
+            Print(StackObjects());
+
+        }
+        public void Print(IEnumerable enumerable)
+        {
+            foreach (var item in enumerable)
+                Console.WriteLine($"\t {item} ");
+            Console.WriteLine();
+
+        }
+    }
+}
